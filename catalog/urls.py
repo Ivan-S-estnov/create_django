66aca0c63d65bea3bde0cb0cc2277dee2590473c
Ivan <jaloylee@yandex.ru>
2025-02-26 18:27:07 +0400

    done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list, product_detail
+from .views import *
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('base/', products_list, name='products_list'),
-    path('product/<int:pk>', product_detail, name='product_detail')
+    path('product/', products_list, name='products_list'),
+    path('product/<int:pk>', product_detail, name='product_detail'),
+    path('newmodel/', NewModelListView.as_view(), name='newmodel_list'),
+    path('newmodel/new/', NewModelCreateView.as_view(), name='newmodel_create'),
+    path('newmodel/<int:pk>', NewModelDetailView.as_view(), name='newmodel_detail'),
+    path('newmodel/<int:pk>/update/', NewModelUpdateView.as_view(), name='newmodel_update'),
+    path('newmodel/<int:pk>/delete/', NewModelDeleteView.as_view(), name='newmodel_delete'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,54 @@
-from itertools import product
-
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from catalog.models import Product
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from catalog.models import Product, NewModel
+
+
+class NewModelCreateView(CreateView):
+    model = NewModel
+    fields = ["name", "description"]
+    template_name = "catalog/newmodel_form.html"
+    success_url = reverse_lazy("catalog:newmodel_list")
+
+
+class NewModelListView(ListView):
+    model = NewModel
+    template_name = "catalog/newmodel_list.html"
+    context_object_name = "newmodels"
+
+
+
+
+class NewModelDetailView(DetailView):
+    model = NewModel
+    template_name = "catalog/newmodel_detail.html"
+    context_object_name = "newmodel"
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
+
+
+class NewModelUpdateView(UpdateView):
+    model = NewModel
+    fields = ["name", "description"]
+    template_name = "catalog/newmodel_form.html"
+    success_url = reverse_lazy("catalog:newmodel_list")
+
+    def get_success_url(self):
+        return reverse("catalog:newmodel_detail", args=[self.kwargs.get("pk")])
+
+
+class NewModelDeleteView(DeleteView):
+    model = NewModel
+    template_name = "catalog/newmodel_confirm_delete.html"
+    success_url = reverse_lazy("catalog:newmodel_list")
+
 
 def home(request):
     return render(request, "home.html")
@@ -18,10 +64,11 @@ def contacts(request):
 
 def products_list(request):
     products = Product.objects.all()
-    context = {'products': products}
+    context = {"products": products}
     return render(request, "product_list.html", context)
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
+    context = {"product": product}
     return render(request, "product_detail.html", context)
