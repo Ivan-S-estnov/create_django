3065f99ea0a085e4a5cd808cbc701faf27bc1645
Ivan <jaloylee@yandex.ru>
2025-02-27 18:43:31 +0400

    all

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,10 +14,5 @@ urlpatterns = [
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 
-    path('newmodel/', NewModelListView.as_view(), name='newmodel_list'),
-    path('newmodel/new/', NewModelCreateView.as_view(), name='newmodel_create'),
-    path('newmodel/<int:pk>', NewModelDetailView.as_view(), name='newmodel_detail'),
-    path('newmodel/<int:pk>/update/', NewModelUpdateView.as_view(), name='newmodel_update'),
-    path('newmodel/<int:pk>/delete/', NewModelDeleteView.as_view(), name='newmodel_delete'),
 
 ]
